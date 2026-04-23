944041e0ad5a2f38b81947cb2163c35391958408
Itay Katav <ikatav@redhat.com>
2026-04-23 11:38:48 +0300

    bugfix: fix the grpc import
    Signed-off-by: Itay Katav <ikatav@redhat.com>

--- app/backend/runtime.py
+++ app/backend/runtime.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2
 import os
 import time
+from urllib.parse import urlparse
 from ovmsclient import make_grpc_client
 
 import tritonclient.grpc as triton_grpc
 
 from logger import get_logger
-from openshift_grpc_url import model_url_to_ovms_grpc_target_openshift
 from response import Detection, postprocess_image, _raw_prediction_tensor
 
 log = get_logger(__name__)
@@ -65,7 +65,13 @@ class Runtime:
             self._infer_output = triton_grpc.InferRequestedOutput("output0")
             self.inference_fun = self.kserve_inference_grpc
         elif openshift_mode:
-            grpc_url = model_url_to_ovms_grpc_target_openshift(self.service_url)
+            parsed = urlparse(self.service_url)
+            if parsed.hostname:
+                host = parsed.hostname
+                port = parsed.port or 9000
+                grpc_url = f"{host}:{port}"
+            else:
+                grpc_url = self.service_url
             self._grpc_client = make_grpc_client(grpc_url)
             self.inference_fun = self.remote_inference
         else:
